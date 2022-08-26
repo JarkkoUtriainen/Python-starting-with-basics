fc63cd152cead8716a1ff45b455cb4c698d0b5f0
JarkkoUtriainen <jarkko.utriainen@metropolia.fi>
2022-08-26 15:20:55 +0300

    Added new folder and assightment 1

---
+++ 3. Valintarakenne (if)/1_kuha.py
@@ -0,0 +1,17 @@
+#user input
+lenght = float(input('Pike-perch lenght: '))
+
+#if lenght is over 37
+if lenght>=37:
+
+    #results
+    print('All good.')
+
+#if lenght is under 37
+if lenght<=37:
+
+    #calculate missing lenght
+    missing = 37-lenght
+
+    #results
+    print(f'Too small. It needs to grow {missing}')
