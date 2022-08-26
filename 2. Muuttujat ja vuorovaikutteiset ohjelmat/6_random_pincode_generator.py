5bd6fe457eda05d1983cb0dc92915617bf8e8776
JarkkoUtriainen <jarkko.utriainen@metropolia.fi>
2022-08-26 14:11:50 +0300

    Added assigment 6

---
+++ 2. Muuttujat ja vuorovaikutteiset ohjelmat/6_random_pincode_generator.py
@@ -0,0 +1,12 @@
+import random
+
+value_1 = str(random.randint(0,9))
+value_2 = str(random.randint(0,9))
+value_3 = str(random.randint(0,9))
+
+value_10 = str(random.randint(1,6))
+value_20 = str(random.randint(1,6))
+value_30 = str(random.randint(1,6))
+value_40 = str(random.randint(1,6))
+
+print(f'\nYour tree number pincode with values between 0-9 is: {value_1}, {value_2}, {value_3}\n\nYour four number pincode with values between 1-6 is: {value_10}, {value_20}, {value_30}, {value_40}')
