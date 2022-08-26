bf8419e061c66a02d91f1c099a11e67b78fff2ad
JarkkoUtriainen <jarkko.utriainen@metropolia.fi>
2022-08-26 15:26:10 +0300

    bug found and fixed

--- 3. Valintarakenne (if)/1_kuha.py
+++ 3. Valintarakenne (if)/1_kuha.py
@@ -7,8 +7,8 @@ if lenght>=37:
     #results
     print('All good.')
 
-#if lenght is under 37
-if lenght<=37:
+#if lenght is under 37 and not equal
+if 37!=lenght<=37:
 
     #calculate missing lenght
     missing = 37-lenght
