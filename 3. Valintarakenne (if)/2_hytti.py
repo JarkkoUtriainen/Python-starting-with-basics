bb423517534715a89d32b9979074caea0b9d212a
JarkkoUtriainen <jarkko.utriainen@metropolia.fi>
2022-08-27 14:27:11 +0300

    added assignment 2

---
+++ 3. Valintarakenne (if)/2_hytti.py
@@ -0,0 +1,19 @@
+#user input
+cabin_class = str(input('Anna hytti luokka ja paina ENTER: '))
+
+#if statements for different option
+if cabin_class == 'LUX' or cabin_class == 'lux':
+    print(f'LUX on parvekkeellinen hytti yläkannella.')
+
+elif cabin_class == 'A' or cabin_class == 'a':
+    print(f'A on ikkunallinen hytti autokannen yläpuolella.')
+
+elif cabin_class == 'B' or cabin_class == 'b':
+    print(f'B on ikkunaton hytti autokannen yläpuolella.')
+
+elif cabin_class == 'C' or cabin_class == 'c':
+    print(f'C on ikkunaton hytti autokannen alapuolella.')
+
+#wrong input
+else:
+    print(f'Virheellinen hyttiluokka')
