500f8fcdffd9971f3528ce73fa2a72f5a95f855b
JarkkoUtriainen <jarkko.utriainen@metropolia.fi>
2022-09-06 20:59:16 +0300

    added assignment 1

---
+++ 4. Alkuehdollinen toistorakenne (while)/1_3_to_1000.py
@@ -0,0 +1,17 @@
+# starting values
+stop = 0
+number = 0
+
+# simple loop
+while stop < 1:
+
+    # stops at 999 because 999 + 3 = 1002 > 1000
+    if number < 999:
+
+        number = number + 3
+
+        print(number)
+
+    else:
+
+        stop = stop + 1
