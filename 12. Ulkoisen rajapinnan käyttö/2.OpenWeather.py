6a32ef0ec9e41bb2df44ba7479b7adc9b71cecf1
Jarkko <111981613+JarkkoUtriainen@users.noreply.github.com>
2023-01-07 16:05:02 +0200

    Update 2.OpenWeather.py

--- 12. Ulkoisen rajapinnan käyttö/2.OpenWeather.py
+++ 12. Ulkoisen rajapinnan käyttö/2.OpenWeather.py
@@ -3,7 +3,7 @@ import json
 
 city_name = input('Give name of municipality: ')
 
-API_key = "8b98bc01e0677287cb58624af62d1cbf"
+API_key = "API-KEY-Here"
 url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={API_key}"
 response = requests.get(url)
 data = json.loads(response.text)
